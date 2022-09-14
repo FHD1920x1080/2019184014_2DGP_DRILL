0cc587ef0c2cb50728f4c9d32da8fd7793984cb1
FHD1920x1080 <terwq135@gmail.com>
2022-09-14 17:16:27 +0900

    drill 04 완성
    2019184014 배성주
    <수정>

--- 04/graphpaper.py
+++ 04/graphpaper.py
@@ -1,18 +1,13 @@
 import turtle
 
-turtle.penup()
-turtle.goto(0,-100)
-turtle.pendown()
-turtle.forward(500)
-turtle.penup()
-i=0
+i=-1
 while(i<5):
+    turtle.penup()
     turtle.goto(0,100*i)
     turtle.pendown()
     turtle.forward(500)
-    turtle.penup()
     i+=1
-
+turtle.penup()
 turtle.goto(0,-100)
 turtle.left(90)
 i=1
@@ -21,7 +16,6 @@ while(i<=6):
     turtle.forward(500)
     turtle.penup()
     turtle.goto(100*i,-100)
-    turtle.pendown()
     i+=1
 
 turtle.exitonclick()
