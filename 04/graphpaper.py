9bb38c7df0d3fd9c8b1d5899668b2331aead9b8b
FHD1920x1080 <terwq135@gmail.com>
2022-09-19 01:42:29 +0900

    Revert "drill 04 완성"
    This reverts commit 0cc587ef0c2cb50728f4c9d32da8fd7793984cb1.

--- 04/graphpaper.py
+++ 04/graphpaper.py
@@ -1,13 +1,18 @@
 import turtle
 
-i=-1
+turtle.penup()
+turtle.goto(0,-100)
+turtle.pendown()
+turtle.forward(500)
+turtle.penup()
+i=0
 while(i<5):
-    turtle.penup()
     turtle.goto(0,100*i)
     turtle.pendown()
     turtle.forward(500)
+    turtle.penup()
     i+=1
-turtle.penup()
+
 turtle.goto(0,-100)
 turtle.left(90)
 i=1
@@ -16,6 +21,7 @@ while(i<=6):
     turtle.forward(500)
     turtle.penup()
     turtle.goto(100*i,-100)
+    turtle.pendown()
     i+=1
 
 turtle.exitonclick()
